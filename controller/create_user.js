85217ce3245b1fc0853019498818b5e742dc6043
Kaspar <kaspupavel@gmail.com>
2024-04-02 09:59:19 +0300

    Used nodeMailer to send a follow up email upon account creation.

--- controller/create_user.js
+++ controller/create_user.js
@@ -1,5 +1,8 @@
 const bcrypt = require('bcrypt');
 const model = require('../models');
+const nodeMailer = require('nodemailer');
+
+
 
 const register = (req, res) => {
     console.log(req.body);
@@ -16,6 +19,34 @@ const register = (req, res) => {
                 email: registered.email,
                 id: registered.id
             }
+            //! Nodemailer
+            //? SMTP Information
+            const transporter = nodeMailer.createTransport({
+                service: 'gmail',
+                host: 'smtp.gmail.com',
+                port: 587,
+                auth: {
+                    user: 'kaspupavel@gmail.com',
+                    //? Generated password for gmail authentication
+                    pass: 'rpxm yfpf tuli rlet'
+                }
+            });
+            //? Create the mail form
+            const mailOptions = {
+                from: 'kaspupavel@gmail.com',
+                to: registered.email,
+                subject: 'Registration Successful',
+                text: 'Welcome to our website!: localhost:3000/users/login'
+            };
+            //? Send the email
+            transporter.sendMail(mailOptions, (error, info) => {
+                if (error) {
+                    console.log(error);
+                } else {
+                    console.log('Email sent: ' + info.response);
+                }
+            });
+
             //? Display the user data
             console.log(req.session);
             res.json({
